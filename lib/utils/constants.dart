5c7c6a182b36a82aae3372f9b66fc52e0e91179c
JoiceAraujo <joicearaujo10@gmail.com>
2021-01-06 20:17:13 -0400

    [WIP] Create constants values  - constants.dart file created  - Created enums of constant values

---
+++ lib/utils/constants.dart
@@ -0,0 +1,27 @@
+class Constants {}
+
+enum DebtTypes {
+  Services,
+  Electronics,
+  Clothing,
+  Supermarket,
+  CreditCard,
+  Academy,
+  Cheers,
+  House,
+  Transport,
+  Recreation,
+  Restaurant,
+  Travel,
+  Others,
+}
+
+enum DebtStatus {
+  Paid,
+  Pending,
+}
+
+enum CardTypes {
+  Credit,
+  Debit,
+}
